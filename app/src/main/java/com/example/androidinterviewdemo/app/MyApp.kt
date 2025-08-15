2dddfa6fb348bc46a76177860afaba0432d68e98
zuozuozuo-cz <542679800@qq.com>
2025-08-15 18:49:59 +0800

    封装 线程工具类

--- app/src/main/java/com/example/androidinterviewdemo/app/MyApp.kt
+++ app/src/main/java/com/example/androidinterviewdemo/app/MyApp.kt
@@ -1,17 +1,37 @@
 package com.example.androidinterviewdemo.app
 
 import android.app.Application
-import com.alibaba.android.arouter.BuildConfig
-import com.alibaba.android.arouter.launcher.ARouter
+import com.example.base_lib.executors.AppExecutors
 
-class MyApp :Application() {
+class MyApp : Application() {
+
+    lateinit var appExecutors: AppExecutors
+        private set // 只允许MyApp 内部赋值
 
     override fun onCreate() {
         super.onCreate()
-        if(BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             ARouter.openLog()
             ARouter.openDebug()
         }
         ARouter.init(this)
+
+        appExecutors = AppExecutors()
     }
+    // companion object  是kotlin的静态对象 相当于 Java 的static
+    companion object {
+        // 提供全局访问 AppExecutors 的方法
+        // 延迟初始化的全局变量
+        lateinit var instance: MyApp
+            private set
+    }
+
+    init {
+        // 每次创建MyApp对象的时候，将自己赋值给instance
+        // 由于Application 只会创建一次，所以instance 实际上就是全局唯一的
+        instance = this
+    }
+
+
 }
+
---
+++ base_lib/src/main/java/com/example/base_lib/executors/AppExecutors.kt
@@ -0,0 +1,26 @@
+package com.example.base_lib.executors
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class AppExecutors (
+    // 磁盘 IO 线程，默认单线程执行任务
+    private val diskIO: Executor = Executors.newSingleThreadExecutor(),
+    // 网络 IO 线程，默认规定线程池 3个线程
+    private  val  netWorkIO: Executor = Executors.newFixedThreadPool(3),
+    // 主线程 executor 用于在 UI线程中执行任务
+    private val mainThread: Executor = MainThreadExecutor()
+){
+
+
+    private  class MainThreadExecutor: Executor{
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(p0: Runnable) {
+            mainThreadHandler.post(p0);
+        }
+    }
+
+}
